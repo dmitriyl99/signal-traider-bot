c9635f8b54f4c5d7c14491ed238fc1841ffec420
Dmitriy <dmitriyl1899@gmail.com>
2022-06-27 17:12:24 +0500

    Connect database

---
+++ api/app/migrations/versions/38c859c0a816_create_users_table.py
@@ -0,0 +1,31 @@
+"""Create users table
+
+Revision ID: 38c859c0a816
+Revises: 
+Create Date: 2022-06-18 15:40:25.771080
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '38c859c0a816'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        'users',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('telegram_user_id', sa.Integer),
+        sa.Column('name', sa.String(128)),
+        sa.Column('phone', sa.String(12)),
+        sa.Column('created_at', sa.DateTime)
+    )
+
+
+def downgrade() -> None:
+    op.drop_table('users')
---
+++ api/app/migrations/versions/645acf3ebe36_create_subscriptions_table.py
@@ -0,0 +1,46 @@
+"""Create subscriptions table
+
+Revision ID: 645acf3ebe36
+Revises: 38c859c0a816
+Create Date: 2022-06-18 16:42:55.527419
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '645acf3ebe36'
+down_revision = '38c859c0a816'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        'subscriptions',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('name', sa.String(128))
+    )
+
+    op.create_table(
+        'subscription_conditions',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('duration_in_month', sa.Integer),
+        sa.Column('price', sa.Integer),
+        sa.Column('subscription_id', sa.Integer, sa.ForeignKey('subscriptions.id'))
+    )
+
+    op.create_table(
+        'subscription_user',
+        sa.Column('user_id', sa.Integer, sa.ForeignKey('users.id'), primary_key=True),
+        sa.Column('subscription_condition_id', sa.Integer, sa.ForeignKey('subscription_conditions.id'), primary_key=True),
+        sa.Column('subscription_id', sa.Integer),
+        sa.Column('active', sa.Boolean, default=False),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table('subscription_user')
+    op.drop_table('subscription_conditions')
+    op.drop_table('subscriptions')
