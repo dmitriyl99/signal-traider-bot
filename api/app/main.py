a215ae1cf12907021d8f3908bc4b394476135f7d
Dmitriy <dmitriyl1899@gmail.com>
2022-06-28 18:00:13 +0500

    Add cors middleware

--- api/app/main.py
+++ api/app/main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers import auth
 
 
 app = FastAPI(title='Signal Traider Bot')
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(
     auth.router,
