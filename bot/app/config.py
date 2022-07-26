644958c75365940705aa2a3e845213a3daf39e82
Dmitriy <dmitriyl1899@gmail.com>
2022-07-26 11:55:23 +0500

    Add configs and start webhook on production env

--- bot/app/config.py
+++ bot/app/config.py
@@ -5,6 +5,8 @@ result = load_dotenv(os.path.join(os.getcwd(), '.env'))
 
 
 class Config:
+    ENV: str
+
     TELEGRAM_BOT_TOKEN: str
     DATABASE_URL: str
     PAYMENT_PROVIDERS: list
@@ -12,12 +14,18 @@ class Config:
     CLICK_PAYMENT_PROVIDER_TOKEN: str
     PAYME_PAYMENT_PROVIDER_TOKEN: str
 
+    WEBHOOK_PORT: int
+    WEBHOOK_TOKEN: str
+
     def __init__(self):
+        self.ENV = os.getenv('ENV')
         self.TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_TOKEN')
         self.DATABASE_URL = os.getenv('DATABASE_URL')
         self.PAYMENT_PROVIDERS = os.getenv('PAYMENT_PROVIDERS').split(',')
         self.CLICK_PAYMENT_PROVIDER_TOKEN = os.getenv('CLICK_PAYMENT_PROVIDER_TOKEN')
         self.PAYME_PAYMENT_PROVIDER_TOKEN = os.getenv('PAYME_PAYMENT_PROVIDER_TOKEN')
+        self.WEBHOOK_PORT = int(os.getenv('WEBHOOK_PORT'))
+        self.WEBHOOK_TOKEN = os.getenv('WEBHOOK_TOKEN')
 
 
 config = Config()
--- bot/app/main.py
+++ bot/app/main.py
@@ -7,14 +7,27 @@ from app.conversations import registration_conversation, select_subscription_con
 from app.payments import handlers
 from app.config import config
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
-application = ApplicationBuilder().token(config.TELEGRAM_BOT_TOKEN).build()
 
-application.add_handler(registration_conversation.handler)
-application.add_handler(select_subscription_conversation.handler)
-application.add_handlers(handlers.handlers)
+def main():
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO
+    )
+    application = ApplicationBuilder().token(config.TELEGRAM_BOT_TOKEN).build()
 
-application.run_polling()
+    application.add_handler(registration_conversation.handler)
+    application.add_handler(select_subscription_conversation.handler)
+    application.add_handlers(handlers.handlers)
+    if config.ENV == 'production':
+        application.run_webhook(
+            listen='0.0.0.0',
+            port=config.WEBHOOK_PORT,
+            url_path=f'api/telegram-update/{config.WEBHOOK_TOKEN}',
+            webhook_url=f'https://apibot.masspay.uz/api/telegram-update/{config.WEBHOOK_TOKEN}'
+        )
+    else:
+        application.run_polling()
+
+
+if __name__ == '__main__':
+    main()
