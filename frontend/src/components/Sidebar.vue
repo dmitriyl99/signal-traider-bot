7b72d39ee0074512c11c4035dd8cf6126ff3ac6b
Dmitriy <dmitriyl1899@gmail.com>
2022-09-07 11:12:12 +0500

    Hide users and transactions

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -33,18 +33,8 @@
           <!-- Navigation -->
           <ul class="navbar-nav">
             <li class="nav-item">
-              <router-link :to="{name: 'UsersList'}" class="nav-link">
-                <i class="fe fe-user"></i> Пользователи
-              </router-link>
-            </li>
-            <li class="nav-item">
               <router-link :to="{name: 'ListSignals'}" class="nav-link">
                 <i class="fe fe-bell"></i> Сигналы
-              </router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link " :to="{name: 'PaymentsList'}">
-                <i class="fe fe-credit-card"></i> История оплат
               </router-link>
             </li>
             <li class="nav-item">
