9831fa663e306ccf7c0cb5b8b4465fb9e657ab8a
Dmitriy <dmitriyl1899@gmail.com>
2022-11-12 09:22:50 +0500

    fix column names

--- api/app/data/models/payme_transaction.py
+++ api/app/data/models/payme_transaction.py
@@ -6,18 +6,18 @@ import sqlalchemy as sa
 class PaymeTransaction(Base):
     __tablename__ = 'payme_transactions'
 
-    id = sa.Column('id', sa.BigInteger, primary_key=True),
-    paycom_transaction_id = sa.Column('paycom_transaction_id', sa.String(25)),
-    paycom_time = sa.Column('paycom_time', sa.String(13)),
-    paycom_time_datetime = sa.Column('paycom_time_datetime', sa.DateTime),
-    create_time = sa.Column('create_time', sa.DateTime),
-    perform_time = sa.Column('perform_time', sa.DateTime, nullable=True),
-    cancel_time = sa.Column('cancel_time', sa.DateTime, nullable=True),
-    amount = sa.Column('amount', sa.Integer),
-    state = sa.Column('state', sa.SmallInteger),
-    reason = sa.Column('reason', sa.SmallInteger, nullable=True),
-    receivers = sa.Column('receivers', sa.String(500), nullable=True, comment='JSON array of receivers'),
-    payment_id = sa.Column('payment_id', sa.BigInteger)
+    id = sa.Column(sa.BigInteger, primary_key=True),
+    paycom_transaction_id = sa.Column(sa.String(25)),
+    paycom_time = sa.Column(sa.String(13)),
+    paycom_time_datetime = sa.Column(sa.DateTime),
+    create_time = sa.Column(sa.DateTime),
+    perform_time = sa.Column(sa.DateTime, nullable=True),
+    cancel_time = sa.Column(sa.DateTime, nullable=True),
+    amount = sa.Column(sa.Integer),
+    state = sa.Column(sa.SmallInteger),
+    reason = sa.Column(sa.SmallInteger, nullable=True),
+    receivers = sa.Column(sa.String(500), nullable=True, comment='JSON array of receivers'),
+    payment_id = sa.Column(sa.BigInteger)
 
 
 class PaymeTransactionStates:
