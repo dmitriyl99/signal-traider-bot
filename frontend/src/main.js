43adda3f30a2f083ed646d11506d6b6a6f09f679
dmitriyl99 <dmitriyl1899@gmail.com>
2023-01-31 18:51:04 +0500

    added mask for currency pairs

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,7 @@ import axios from 'axios'
 import Select2 from 'vue3-select2-component';
 import VueAwesomePaginate from "vue-awesome-paginate";
 import "vue-awesome-paginate/dist/style.css";
+import VueTheMask from 'vue-the-mask'
 
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
@@ -18,5 +19,5 @@ axios.defaults.baseURL = 'http://localhost:8000';
 
 const app = createApp(App).use(store).use(router)
 app.component('Select2', Select2)
-app.use(VueSweetalert2).use(VueAwesomePaginate);
+app.use(VueSweetalert2).use(VueAwesomePaginate).use(VueTheMask);
 app.mount('#app')
