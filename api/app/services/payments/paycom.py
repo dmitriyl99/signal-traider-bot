dedbab595fadd3086f5a50f443fe80a5acf433c8
dmitriyl99 <dmitriyl1899@gmail.com>
2023-12-08 01:09:59 +0500

    Fix timestamp

--- api/app/services/payments/paycom.py
+++ api/app/services/payments/paycom.py
@@ -157,9 +157,10 @@ class PaycomPaymentHandler:
             self.data.params['amount'],
             self.data.params['account']['order_id']
         )
+        timestamp = transaction.create_time.timestamp() * 1000000
 
         return {
-            'create_time': transaction.create_time,
+            'create_time': int(timestamp),
             'transaction': transaction.id,
             'state': transaction.state,
             'receivers': None
