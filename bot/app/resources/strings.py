a7e8a7d341dced355a8d4032175fdf53f30aac20
Dmitriy Lijin <dmitriyl1899@gmail.com>
2024-08-09 14:45:31 +0500

    Change strings

--- bot/app/resources/strings.py
+++ bot/app/resources/strings.py
@@ -1,206 +1,168 @@
 strings_data = {
     'registration_language': {
-        'ru': '🇷🇺 Пожалуйста, выберите язык\n\n🇺🇿 Iltimos, tilni tanlang',
-        'uz': '🇷🇺 Пожалуйста, выберите язык\n\n🇺🇿 Iltimos, tilni tanlang'
+        'ўз': '🇺🇿 Илтимос, тилни танланг\n\n🇺🇿 Iltimos, tilni tanlang',
+        'uz': '🇺🇿 Илтимос, тилни танланг\n\n🇺🇿 Iltimos, tilni tanlang'
     },
     'registration_language_wrong': {
-        'ru': 'Выбран неправильный язык',
+        'ўз': 'Нотўғри тил танланган',
         'uz': "Noto'g'ri til tanlangan"
     },
     'registration_name': {
-        'ru': "🤝Прежде чем мы начнем, давайте узнаем друг друга получше. Как мне к вам обращаться? Напишите свое имя в чат, как будто мы старые друзья – просто и непринужденно, например 'Тимур' или 'Мария'. 🔽",
+        'ўз': "🤝Ишимиз бошланишидан аввал келинг яқинроғдан танишайлик. Сизга қандай мурожаат қилсам бўлади? Чатга исмингизни ёзинг, сиз билан дўстлардек яқинликда бўлайлик– барчаси оддий, “Тимур” ёки “Мадина'. 🔽",
         'uz': "🤝Ishimiz boshlanishidan avval keling yaqinrog’dan tanishaylik. Sizga qanday murojaat qilsam bo’ladi? Chatga ismingizni yozing, siz bilan do’stlardek yaqinlikda bo’laylik– barchasi oddiy, “Timur” yoki “Madina”. 😊"
     },
     'registration_phone': {
-        'ru': "Отлично, теперь нам нужен ваш номер телефона для отправки SMS с кодом регистрации. Пожалуйста, введите его здесь, и мы сразу же отправим вам сообщение для продолжения 🔽",
+        'ўз': "Ажойиб, энди рўйхатдан ўтиш кодини СМС орқали юбориш учун сизнинг телефон рақамингиз керак. Илтимос, уни бу ерга киритинг, ва биз дарҳол сизга давом этиш учун хабар юборамиз 🔽",
         'uz': "Ajoyib, endi ro'yxatdan o'tish kodini SMS orqali yuborish uchun sizning telefon raqamingiz kerak. Iltimos, uni bu yerga kiriting, va biz darhol sizga davom etish uchun xabar yuboramiz 🔽"
     },
     'registration_phone_not_verified': {
-        'ru': 'Вы ещё не потвердили свой номер телефона. Мы выслали вам код, введите его здесь пожалуйста🔽',
+        'ўз': 'Сиз ҳали телефон рақамингизни тасдиқламадингиз. Сизга СМС орқали код юбордик, илтимос, уни киритинг🔽',
         'uz': 'Siz hali telefon raqamingizni tasdiqlamadingiz. Sizga SMS orqali kod yubordik, iltimos, uni kiriting'
     },
     'registration_phone_user_exists': {
-        'ru': 'Пользовтаель с этим номером телефона уже существует',
+        'ўз': 'Ушбу телефон рақамига эга фойдаланувчи аллақачон мавжуд',
         'uz': 'Ushbu telefon raqamiga ega foydalanuvchi allaqachon mavjud'
     },
     'registration_phone_otp_sent': {
-        'ru': 'Мы выслали вам код, введите его здесь пожалуйста🔽',
+        'ўз': 'Биз сизга кодни юбордик, илтимос, уни бу ерга киритинг 🔽',
         'uz': 'Biz sizga kodni yubordik, iltimos, uni bu yerga kiriting 🔽'
     },
     'registration_phone_otp_wrong_format': {
-        'ru': 'Вы отправили неверный формат OTP',
+        'ўз': 'Сиз нотўғри ОТП форматини юбордингиз',
         'uz': "Siz noto'g'ri OTP formatini yubordingiz"
     },
     'registration_phone_otp_wrong': {
-        'ru': 'Вы отправили неверный OTP',
+        'ўз': 'Сиз нотўғри ОТП юбордингиз',
         'uz': "Siz noto'g'ri OTP yubordingiz"
     },
     'registration_proactively': {
-        'ru': '{name}, вы активировали подписку!',
+        'ўз': '{name}, сиз обунани фаоллаштирдингиз!',
         'uz': '{name}, siz obunani faollashtirdingiz!'
     },
     'registration_finished': {
-        'ru': 'Спасибо, регистрация завершена!',
+        'ўз': 'Раҳмат, рўйхатдан ўтиш тугади!',
         'uz': "Rahmat, ro'yxatdan o'tish tugadi!"
     },
     'registration_bonus_activated': {
-        'ru': 'Вы активировали бонусную подписку!',
+        'ўз': 'Сиз бонусли обунани фаоллаштирдингиз!',
         'uz': 'Siz bonusli obunani faollashtirdingiz!'
     },
     'subscription_menu_message': {
-        'ru': """А теперь к самому доступу в комьюнити! Вот доступные тарифы для подписки:
-🔘 1 месяц - <b>50$</b> 💵""",
-        'uz': """Endi <b>OneZone</b> hamjamiyatiga kirish haqida! Mana obuna uchun mavjud tariflar:
+        'ўз': """Энди  Isaev Full Contact  ҳамжамиятига кириш ҳақида! Мана обуна учун мавжуд тарифлар:
+🔘 1 ой - <b>50$</b> 💵""",
+        'uz': """Endi <b>Isaev Full Contact</b> hamjamiyatiga kirish haqida! Mana obuna uchun mavjud tariflar:
 🔘 1 oy - <b>50$</b> 💵"""
-    },
-    'subscription_menu_message_2': {
-        'ru': """<b>Но это еще не все!</b> 🌟 В OneZone есть две группы: одна на <b>русском</b> языке от <b>Амаля</b> и другая на <b>узбекском</b> языке от <b>Захриддина</b>. Если вы владеете узбекским языком, вы можете <B>подписаться</b> на <b>обе</b> группы и получить <b>двойную аналитику</b>! Специальное предложение за две подписки:
-🔘 1 месяц: <b>20$</b> (10% скидка) 💵
-🔘 3 месяца: <b>56$</b> (18.67$ за месяц) 💵
-🔘 6 месяцев: <b>106$</b> (17.67$ за месяц) 💵
-🔘 12 месяцев: <b>202$</b> (16.83$ за месяц) 💵 
-
-Получите <b>двойную аналитику</b> и увеличьте свои <b>возможности</b> в трейдинге на <b>Forex!</b>""",
-        'uz': """<b>Bu hali hammasi emas!</b> 🌟 OneZone'da ikkita guruh mavjud: biri rus tilida Amaldan va ikkinchisi o'zbek tilida Zahridindan. Agar siz o'zbek tilida so'zlasha olsangiz, ikkala guruhga ham obuna bo'lishingiz va ikki baravar ko'proq tahlil olishingiz mumkin! Ikki guruh uchun maxsus taklif:
-🔘 1 oy: <b>20$</b> (10% chegirma) 💵
-🔘 3 oy: <b>56$</b> (oyiga 18.67$) 💵
-🔘 6 oy: <b>106$</b> (oyiga 17.67$) 💵
-🔘 12 oy: <b>202$</b> (oyiga 16.83$) 💵 
-Ikki baravar tahlil oling va Forex'dagi treyding imkoniyatlaringizni oshiring!
-"""
     },
 
     'choose_subscription_text': {
-        'ru': 'Выбрать подписку',
+        'ўз': 'Обунани танланг',
         'uz': 'Obunani tanlang'
     },
     'graphical_signals': {
-        'ru': 'OneZone группа - аналитические сигналы',
-        'uz': 'OneZone группа - аналитические сигналы'
+        'ўз': 'Isaev Full Contact группа - аналитические сигналы',
+        'uz': 'Isaev Full Contact группа - аналитические сигналы'
     },
     'interday_subscriptions': {
-        'ru': 'Interday',
+        'ўз': 'Interday',
         'uz': 'Interday'
     },
     'marafon_subscriptions': {
-        'ru': 'Marafon',
+        'ўз': 'Marafon',
         'uz': 'Marafon'
     },
     'hello_message': {
-        'ru': 'Здравствуйте, %s!',
+        'ўз': 'Салом, %s!',
         'uz': 'Salom %s!'
     },
     'send_phone_button_text': {
-        'ru': 'Отправить номер',
+        'ўз': 'Рақамни юбориш',
         'uz': 'Raqamni yuborish'
     },
     'wrong_number_button_text': {
-        'ru': 'Неправильный номер',
+        'ўз': 'Нотўғри рақам',
         'uz': "Noto'g'ri raqam"
     },
     'validation_phone_message': {
-        'ru': 'Неправильный формат номер',
+        'ўз': 'Нотўғри рақам формати',
         'uz': "Noto'g'ri raqam formati"
     },
     'active_subscription': {
-        'ru': 'У вас имеется подписка {name} до {to_date}. Осталось: {days} дней. Вот ваш эксклюзивный доступ к группе OneZone: {invite_links}.',
-        'uz': '{to_date} gacha {name}ga obunangiz bor. Qolgan kun: {days}. Mana sizning OneZone guruhiga eksklyuziv kirishingiz: {invite_links}.'
+        'ўз': 'Сизда обунангиз бор {name} {to_date} гача. Қолган кун: {days} .Мана сизнинг Isaev Full Contact гуруҳига эксклюзив киришингиз: {invite_links}.',
+        'uz': '{to_date} gacha {name}ga obunangiz bor. Qolgan kun: {days}. Mana sizning Isaev Full Contact guruhiga eksklyuziv kirishingiz: {invite_links}.'
     },
 
     'subscription_not_found': {
-        'ru': 'Такая подписка не найдена',
+        'ўз': 'Бундай обуна топилмади',
         'uz': 'Bunday obuna topilmadi'
     },
     'subscription_condition_wrong': {
-        'ru': 'Выбрано не праивльное условие подписки',
+        'ўз': 'Нотўғри обуна бўлиш шарти танланган',
         'uz': "Noto'g'ri obuna bo’lish sharti tanlangan"
     },
     'back_button': {
-        'ru': 'Назад',
+        'ўз': 'Орқага',
         'uz': 'Orqaga'
     },
     'subscription_condition_name_months': {
-        'ru': '%s месяц',
+        'ўз': '%s ой',
         'uz': '%s oy'
     },
     'subscription_condition_name_days': {
-        'ru': '%s дней',
+        'ўз': '%s кун',
         'uz': '%s kun'
     },
     'leave_application_online': {
-        'ru': 'Оставить заявку онлайн',
+        'ўз': 'Онлайн сўров қолдиринг',
         'uz': "Onlayn so'rov qoldiring"
     },
     'subscription_month': {
-        'ru': 'месяц',
+        'ўз': 'ой',
         'uz': 'oy'
     },
     'subscription_days': {
-        'ru': 'дней',
+        'ўз': 'кун',
         'uz': 'kun'
     },
     'provider_provider_not_supported': {
-        'ru': 'Выбран провайдер, которого мы не поддерживаем',
+        'ўз': 'Биз қўллаб-қувватламайдиган провайдер танланган',
         'uz': "Biz qo'llab-quvvatlamaydigan provayder tanlangan"
     },
     'payment_pay_subscription': {
-        'ru': 'Оплатить подписку',
+        'ўз': 'Обуна учун тўлаш',
         'uz': "Obuna uchun to'lash"
     },
     'payment_subscription_info': {
-        'ru': '%s на %d месяцев за $%d',
+        'ўз': '%s на % ой давомида  $%d учун',
         'uz': '%s %d oy davomida $%d'
     },
     'payment_cancelation_button': {
-        'ru': 'Для отмены нажмите кнопку "Назад"',
+        'ўз': 'Бекор қилиш учун орқага тугмасини босинг',
         'uz': 'Bekor qilish uchun orqaga tugmasini bosing'
     },
     'subscription_select_condition': {
-        'ru': 'Выберите срок подписки',
+        'ўз': 'Обуна муддатини танланг',
         'uz': 'Obuna muddatini tanlang'
     },
     'subscription_full_info': {
-        'ru': '<b>Подписка:</b> {}\n<b>Срок:</b> {}\n<b>Цена:</b> ${}',
+        'ўз': '<b>Обуна:</b> {}\n<b>Муддат:</b> {}\n<b>Нарх:</b> ${}',
         'uz': '<b>Obuna:</b> {}\n<b>Muddat:</b> {}\n<b>Narx:</b> ${}'
     },
-    'subscription_purchased': {
-        'ru': 'Подписка куплена!',
-        'uz': 'Obuna sotib olindi!'
-    },
     'subscription_pay': {
-        'ru': '💳 Оплатить',
-        'uz': "💳 to'lash"
+        'ўз': '💳 Тўлаш',
+        'uz': "💳 To'lash"
     },
     'language_changed': {
-        'ru': '🇷🇺 Язык изменён',
+        'ўз': '🇷🇺 Тил ўзгартирилди',
         'uz': "🇺🇿 Til o'zgartirildi"
     },
     'subscription_purchased': {
-        'ru': 'Поздравляем с успешной оплатой подписки! 🎉 Вот ваш эксклюзивный доступ к группе OneZone: {invite_links}. Присоединяйтесь и начните свой путь к успеху на Forex сегодня!',
-        'uz': "Tabriklaymiz, obuna uchun to'lov muvaffaqiyatli amalga oshirildi! 🎉 Mana sizning OneZone guruhiga eksklyuziv kirishingiz: {invite_links}. Qo'shiling va bugun Forex'dagi muvaffaqiyat yo'lingizni boshlang!"
+        'ўз': 'Табриклаймиз, обуна учун тўлов муваффақиятли амалга оширилди! 🎉 Мана сизнинг Isaev Full Contact гуруҳига эксклюзив киришингиз: {invite_links}. Қўшилинг ва бугун Форехъдаги муваффақият йўлингизни бошланг',
+        'uz': "Tabriklaymiz, obuna uchun to'lov muvaffaqiyatli amalga oshirildi! 🎉 Mana sizning Isaev Full Contact guruhiga eksklyuziv kirishingiz: {invite_links}. Qo'shiling va bugun Forex'dagi muvaffaqiyat yo'lingizni boshlang!"
     },
     'invite_group': {
-        'ru': 'Ссылка на Группу {name}',
+        'ўз': '{name} Гуруҳига Ҳавола',
         'uz': '{name} Guruhiga Havola'
     },
-    'welcome_text': {
-        'ru': """🌐 В OneZone вы найдете: 
-
-📊 <b>Анализы рынка Forex</b>: Видеоразборы аналитики и прогнозы роста цен для точных торговых решений.
-📰 <b>Недельные новости Forex</b>: Сигналы о высоких рисках, предупреждающие о неподходящем времени для открытия сделок. 
-📚 <b>Обучающие курсы</b>: Комплексные ресурсы для новичков и опытных трейдеров, научат вас всему необходимому для успешного трейдинга. 
-🔗 <b>Лайфхаки и полезные ссылки</b>: Советы и ресурсы для эффективной торговли. 
-🤝 <b>Доступ к комьюнити трейдеров</b>: Общайтесь и учитесь у опытных участников сообщества."
-""",
-        'uz': """🌐 OneZone'da topasiz: 
-
-📊 <b>Forex Bozor Tahlillari</b>: Jonli efir da jonli savdo va grafik tahlillari 📈
-📰 <b>Forex Haftalik Yangiliklari</b>: Ta’sir darajasi yuqori yangiliklar. 
-📚 <b>O'quv Kurslari</b>: Yangi boshlovchilar va tajribali treyderlar uchun o’quv resurslari, sizni muvaffaqiyatga olib boruvchi tajriba darslari. 
-🔗 <b>Foydali Maslahatlar va Havolalar</b>: Samarali savdo uchun maslahatlar va resurslar. 
-🤝 <b>Treyderlar Hamjamiyatiga Kirish</b>: Tajribali ishtirokchilar bilan muloqot qiling va ulardan o'rganing.
-Dunyodagi eng qimmat narsalardan biri bu “fikr almashish”"""
-    }
 }
 
 
